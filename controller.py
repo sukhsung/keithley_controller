9cac00a5c976624ad08a1058cc341ed974e666ef
Suk Hyun Sung <sukhsung@umich.edu>
2021-06-25 17:34:25 -0400

    started fresh

--- controller.py
+++ controller.py
@@ -1,188 +1,94 @@
 import pyvisa as visa
-import numpy as np
-import time
-import os
 
-def setup( model_name, terminal = "REAR", use_pyvisapy = True ):
+class controller:
+    
+    def __init__(self, device_address, use_pyvisapy = True, verbose = False):
+        self.device_address = device_address
+        self.verbose = verbose
+        # INITIALIZE VISA
+        if use_pyvisapy:
+            self.resource_manager = visa.ResourceManager("@py")
+            print( "Using pyvisa-py backend\n" )
+        else :
+            self.resource_manager = visa.ResourceManager()
 
-    if model_name == "hlab_2450":
-        model = "USB0::0x05E6::0x2450::04451534::INSTR"
-    elif model_name == "hlab_2110":
-        model = "USB0::0x05E6::0x2110::8011648::INSTR"
-    else :
-        print( "Unsupported Model" )
+        
+        self.device = self.resource_manager.open_resource( device_address )    
+        self.reset()
 
-    if ( terminal == "FRONT" ):
-        terminal = "FRON"
+    def current_device(self) :
+        print(self.device.query("*IDN?"))
 
-    if ( terminal != "REAR" and terminal !="FRON" ):
-        print( "Unsupported Terminal" )
+    def write( self, command ):
+        if self.verbose :
+            print("    {}".format(command))
+        self.device.write(command)
 
-    # INITIALIZE VISA
-    if use_pyvisapy:
-        rm = visa.ResourceManager("@py")
-        print( "Using pyvisa-py backend" )
-    else :
-        rm = visa.ResourceManager()
-
-    # INITIALIZING AND COMMUNICATING WITH KEITHLEY
-    keithley = rm.open_resource( model )    
-    keithley.write("*RST; :STAT:PRES; *CLS")
-    keithley.write(":OUTPut:STATe OFF")
-    keithley.write(":SYST:BEEP {},{}".format(500,0.2))
-    keithley.write(":ROUT:TERM {}".format(terminal))
-    print( "Using {} terminal".format(terminal) )
+    def query( self, command ):
+        if self.verbose :
+            print("    {}".format(command))
+        return self.device.query(command)
     
 
-    return keithley
+    def reset( self ) :
+        print( "Resetting..." )
+        self.write("*RST; :STAT:PRES; *CLS")
+        self.write(":OUTP OFF")
+        self.write(":OUTP:STAT OFF")
+        self.write(":SYST:BEEP {},{}".format(500,0.2))
+        print( "\n" )
 
-def list_devices( ):
-    rm = visa.ResourceManager("@py")
-    rm.list_resources()
 
-def current_sweep( keithley ):
 
-    # Setting Current Source
-    keithley.write(":SOUR:FUNC: CURR")
-    
+class keithley_2110( controller ):
+    def __init__(self, serial_number, use_pyvisapy = True, verbose = False):
+        address = "USB0::0x05E6::0x2110::{}::INSTR".format(serial_number)
+        controller.__init__(self, address, use_pyvisapy, verbose)
+        self.serial_number = 8011648
 
-def measure_resistance( keithley, FOUR_OR_TWO, source_current, NUM_SAMPLE, READ_INTERVAL, NUM_MEASUREMENT ):
+    def measure_resistance( self, FOUR_OR_TWO, RESISTANCE_RANGE="AUTO" ):
 
-    # Switch to 4-Point Measurement
-    if FOUR_OR_TWO == "4PT":
-        keithley.write(":SENS:VOLT:RSEN ON")
-        print( "4 Point Resistance Measurement:")
-        print( "FH - SH - Device - SL - FL" )
-    elif FOUR_OR_TWO == "2PT":
-        keithley.write(":SENS:VOLT:RSEN OFF")
-        print( "2 Point Resistance Measurement:")
-        print( "FH - Device - FL" )
-    else:
-        print( "Unsupported Mode")
+        # Set 4-point or 2-point measurement
+        if FOUR_OR_TWO == 4:
+            RES = "FRES"
+        elif FOUR_OR_TWO == 2:
+            RES = "RES"
+        self.write("FUNC \"{}\"".format(RES))
+
+        if RESISTANCE_RANGE == "AUTO":
+            self.write("{}:RANG:AUTO ON".format(RES))
+        elif (RESISTANCE_RANGE >=0 and RESISTANCE_RANGE <= 100e6):
+            self.write("{}:RANG {}".format(RES,RESISTANCE_RANGE))
+
+        data = self.query( "READ?" )
+        return data
+
+    def measure_capacitance( self, CAPACITANCE_RANGE="AUTO"):
+        self.write("FUNC \"CAP\"")
+
+        if CAPACITANCE_RANGE == "AUTO":
+            self.write("CAP:RANG:AUTO ON")
+        elif (CAPACITANCE_RANGE >=0 and CAPACITANCE_RANGE <= 10e-3):
+            self.write("CAP:RANG {}".format(CAPACITANCE_RANGE))
+
+        data = self.query( "READ?" )
+        return data
+
+class keithley_2450( controller ):
+    def __init__(self, serial_number, use_pyvisapy = True, verbose = False):
+        address = "USB0::0x05E6::0x2450::{}::INSTR".format(serial_number)
+        controller.__init__(self, address, use_pyvisapy, verbose)
+        self.serial_number = 8011648
+
+    def four_point_resistance( self ):
+        return
+    def two_point_resistance( self ):
         return
 
-    ## MEASUREMENT SETUP
+class hlab_2110( keithley_2110 ):
+    def __init__(self, use_pyvisapy = True, verbose = False):
+        keithley_2110.__init__(self, "8011648", use_pyvisapy, verbose)
 
-    # Set to source current
-    keithley.write(":SOUR:FUNC CURR")
-    keithley.write(":SOUR:CURR {}".format(source_current))
-    print( "Sourcing Current: {}A\n".format(source_current))
-    
-    # Measure Resistance
-    keithley.write(":SENS:FUNC \"VOLT\"")
-    keithley.write(":SENS:VOLT:UNIT VOLT")
-    keithley.write("SENS:VOLT:RANG:AUTO ON") #### Auto range
-
-    # trigger for taking 3 samples per measurement with read_interveral in between to defbuffer1
-    keithley.write(":TRIG:LOAD \"SimpleLoop\", {}, {}, \"defbuffer1\"".format(NUM_SAMPLE,READ_INTERVAL))
-
-    Vs = []
-    Is = []
-    Ts = []
-    Rs = []
-    time_init = time.time()
-    # Start Measuring
-    header = "Time (s),Source Current (A),Measured Voltage (V),Resistance (Ohm)"
-    print( "[{}]".format(header) )
-    counter = 0
-    while counter < NUM_MEASUREMENT:
-        try:
-            keithley.write("OUTP ON")
-            keithley.write("INIT")
-            keithley.write("*WAI")
-            keithley.write("TRAC:DATA? 1, 1, \"defbuffer1\", SOUR, READ")
-            keithley.write("OUTP OFF")
-            curT = time.time()-time_init
-
-
-            bufferdata = keithley.read()
-            curI = float(bufferdata.split(",")[0] )
-            curV = float(bufferdata.split(",")[1] )
-            curR = curV/curI
-
-            Is.append( curI )
-            Vs.append( curV )
-            Ts.append( curT )
-            Rs.append( curR )
-            print( [curT, curI, curV, curR] )
-            counter += 1
-        except KeyboardInterrupt:
-            print( "KeyboardInterrupt: Ending Early")
-            counter = NUM_MEASUREMENT
-
-    data = np.array( [Ts,Is,Vs,Rs]).transpose()
-
-    return data, header
-
-def voltage_sweep( keithley, EXPERIMENT, SOURCE_VOTLAGES, CURRENT_LIMIT, NUM_SAMPLE, READ_INTERVAL ):
-
-    if EXPERIMENT == "2PT_IV":
-        keithley.write(":SENS:VOLT:RSEN OFF")
-        print( "2 Point Voltage Sweep")
-        print( "FH - Device - FL" )
-    else:
-        print( "Unsupported Mode")
-        return
-
-    ## MEASUREMENT SETUP
-
-    # Set to source voltage
-    keithley.write(":SOUR:FUNC VOLT")
-    keithley.write(":SOUR:CURR:ILIM {}".format(CURRENT_LIMIT))
-    
-    # Measure Resistance
-    keithley.write(":SENS:FUNC \"CURR\"")
-    keithley.write(":SENS:CURR:UNIT AMP")
-    keithley.write("CURR:RANG:AUTO ON") #### Auto range
-
-    # trigger for taking 3 samples per measurement with read_interveral in between to defbuffer1
-    keithley.write(":TRIG:LOAD \"SimpleLoop\", {}, {}, \"defbuffer1\"".format(NUM_SAMPLE,READ_INTERVAL))
-
-    Vs = []
-    Is = []
-    Ts = []
-    time_init = time.time()
-    # Start Measuring
-    header = "Time (s),Source Voltage (V),Measured Current (I)"
-    print( "[{}]".format(header) )
-    for source_v in SOURCE_VOTLAGES:
-        #keithley.write(":SENS:AZER:ONCE")
-        keithley.write(":SOUR:VOLT {}".format(source_v))
-        keithley.write("OUTP ON")
-        keithley.write("INIT")
-        keithley.write("*WAI")
-        keithley.write("TRAC:DATA? 1, 1, \"defbuffer1\", SOUR, READ")
-        keithley.write("OUTP OFF")
-        curT = time.time()-time_init
-
-        bufferdata = keithley.read()
-        curV = float(bufferdata.split(",")[0] )
-        curI = float(bufferdata.split(",")[1] )
-
-        Is.append( curI )
-        Vs.append( curV )
-        Ts.append( curT )
-        print( [curT, curV, curI] )
-
-    data = np.array( [Ts,Vs,Is]).transpose()
-
-    return data, header
-
-def save_as_csv( data, savedir, sample_name, experiment, header ):
-
-    savepath = os.path.join( savedir, sample_name)
-    if not os.path.exists( savepath ):
-        os.makedirs( savepath )
-
-    now = time.localtime()
-    fname = "{}_{}".format(time.strftime("%H%M",now), experiment)
-    
-    fullpath = os.path.join( savepath,fname+".csv" )
-    while os.path.isfile( fullpath ):
-        fname = fname + "_"
-        fullpath = os.path.join( savepath,fname+".csv" )
-
-    f = open( fullpath ,'w')
-    f.write("{}\n".format(header))
-    np.savetxt(f, data, delimiter = ",")
-    f.close()
+class hlab_2450( keithley_2450 ):
+    def __init__(self, use_pyvisapy = True, verbose = False):
+        keithley_2110.__init__(self, "04451534", use_pyvisapy, verbose)
